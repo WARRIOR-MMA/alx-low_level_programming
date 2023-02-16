51683512895c7333202ebd42f7546685bf21ccc2
WARRIOR-MMA <warriorprishen@icloud.com>
2023-02-16 14:42:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program that prints a line to std arrow
+ * main - C program that prints a line
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
 return (1);
 }
