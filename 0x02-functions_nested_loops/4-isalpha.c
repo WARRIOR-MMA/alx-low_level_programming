c530744f0432d917a700f4e819113955f67b0e9f
WARRIOR-MMA <warriorprishen@icloud.com>
2023-02-21 13:15:26 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic char
+ * @c: to be checked if a letter
+ * Return: 1 if c is a letter, otherwise 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
