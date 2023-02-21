ed113d15fc1002747c89048b3e8d43c52482a405
WARRIOR-MMA <warriorprishen@icloud.com>
2023-02-21 14:07:20 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,18 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int a;
 
-	if (last_digit < 0)
-		last_digit *= -1;
-	_putchar(last_digit = '0');
-	return (last_digit);
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+
 }
