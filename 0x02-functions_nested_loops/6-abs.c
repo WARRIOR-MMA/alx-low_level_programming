f2cd453a508404c68ea614976bec3cb8ae1c8bb1
WARRIOR-MMA <warriorprishen@icloud.com>
2023-02-21 13:41:18 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - calculates the abs value of int
+ * @n: int being checked
+ * Return: abs of the int.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
