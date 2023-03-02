4ee3cb767e45e4528f59eae14b4a04917e8eb918
WARRIOR-MMA <warriorprishen@icloud.com>
2023-03-02 01:53:04 -0800

    0x06 C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverses integer array
+ * @a: array
+ * @n: number of elements in array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+	return (0);
+}
