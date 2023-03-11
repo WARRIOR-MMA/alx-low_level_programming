504d8bf283263e8ba01b8985473b9037d6a19010
WARRIOR-MMA <warriorprishen@icloud.com>
2023-03-11 10:05:04 -0800

    updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -27,12 +27,12 @@ int _atoi(char *s)
 	if (s[i] >= '0' && s[i] <= '9')
 	{
 		digit = s[i] - '0';
-	if (d % 2)
+		if (d % 2)
 		digit = -digit;
 		n = n * 10 + digit;
 		f = 1;
 
-	if (s[i + 1] < '0' || s[i + 1] > '9')
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 		break;
 
 		f = 0;
@@ -59,9 +59,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-		num1 = _atoi(argv[1]);
-		num2 = _atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
 	return (0);
 }
+
