9b333f93b48fb87f7b567f6fdc1a65b88749b80c
WARRIOR-MMA <warriorprishen@icloud.com>
2023-03-11 04:51:11 -0800

    Static.sh

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -2,11 +2,13 @@
 /**
  * _atoi - convert a string into an integer.
  *
- * @s: the string to use.
+ * @s: UNUSED string
  *
  * Return: integer.
  */
 int _atoi(char *s)
 {
+	(void)s;
+
 	return (0);
 }
