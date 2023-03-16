26a2f53abcaafc52125c43b18b9b50c23b92ec8d
WARRIOR-MMA <warriorprishen@icloud.com>
2023-03-16 10:55:06 -0700

    Need to revise

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_realloc - relocates mem using malloc and free
+ * @ptr: pointer to mem that was allocated prev by malloc
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
