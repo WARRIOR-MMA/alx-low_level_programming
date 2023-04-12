d6dde39d56177ba6ed579294f97ed8921fd7859c
WARRIOR-MMA <warriorprishen@icloud.com>
2023-04-12 01:35:55 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,37 +2,52 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads file and prints it to POSIX stdout.
- * @filename: Pointer to the name of file.
- * @letters: Number of letters function will read and print.
- * Return: If function fails or filename is NULL - 0.
- * O/w - the actual number of bytes the function can read and print.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The maximum number of bytes the function will read and print.
+ * Return: If the function fails or filename is NULL - 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t c, r, w;
+	ssize_t file_descriptor, bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
+
 	if (buffer == NULL)
 		return (0);
 
-	c = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	file_descriptor = open(filename, O_RDONLY);
 
-	if (c == -1 || r == -1 || w == -1 || w != r)
+	if (file_descriptor == -1)
 	{
 		free(buffer);
 		return (0);
 	}
 
+	bytes_read = read(file_descriptor, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
 	free(buffer);
-	close(c);
+	close(file_descriptor);
 
-	return (w);
+	return (bytes_written);
 }
-
