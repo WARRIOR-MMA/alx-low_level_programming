2e7b6cc7d25357a712c3ca60a41bed9f0ec91bd5
WARRIOR-MMA <warriorprishen@icloud.com>
2023-04-12 01:00:17 -0700

    Correction Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: The name of the file to read.
- * @letters: The number of characters to read.
- * Return: The actual number of characters read and printed, or 0 on failure.
+ * read_textfile - Reads file and prints it to POSIX stdout.
+ * @filename: Pointer to the name of file.
+ * @letters: Number of letters function will read and print.
+ * Return: If function fails or filename is NULL - 0.
+ * O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
+	ssize_t c, r, w;
+	char *buffer;
 
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	c = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (c == -1 || r == -1 || w == -1 || w != r)
 	{
+		free(buffer);
 		return (0);
 	}
 
-	char buf[letters];
-	ssize_t bytes_read = read(fd, buf, letters);
+	free(buffer);
+	close(c);
 
-	if (bytes_read == -1)
-	{
-		close(fd);
-		return (0);
-	}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-
-	close(fd);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		return (0);
-	}
-
-	return (bytes_written);
+	return (w);
 }
+
