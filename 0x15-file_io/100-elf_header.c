5b7fc6876798cd7327c01dee882387627e0ac36a
WARRIOR-MMA <https://github.com/WARRIOR-MMA/alx-low_level_programming.git>
2023-05-08 13:44:50 -0700

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -17,47 +17,4 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
-/**
- * check_elf - To See if file is ELF File
- * @e_ident: An array with pointer containing the ELF magic numbers.
- * Description: If the file is not an ELF file - exit code 98.
- */
-void check_elf(unsigned char *e_ident)
-{
-	int info;
-
-	for (info = 0; info < 4; info++)
-	{
-		if (e_ident[info] != 127 &&
-		    e_ident[info] != 'E' &&
-		    e_ident[info] != 'L' &&
-		    e_ident[info] != 'F')
-		{
-			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-			exit(98);
-		}
-	}
-}
-
-/**
- * print_magic - The ELF header magic numbers will be printed
- * @e_ident: An array that has a pointer containing ELF magic numbers.
- * Description: Magic numbers are separated by spaces.
- */
-void print_magic(unsigned char *e_ident)
-{
-	int info;
-
-	printf("  Magic:   ");
-
-	for (info = 0; info < EI_NIDENT; info++)
-	{
-		printf("%02x", e_ident[info]);
-
-		if (info == EI_NIDENT - 1)
-			printf("\n");
-		else
-			printf(" ");
-	}
-}
-
+/*
