38b68b1c9c08e121905f12bf25960985b1a49ce3
WARRIOR-MMA <warriorprishen@icloud.com>
2024-02-17 00:36:24 -0800

    100-elf correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,39 +4,46 @@
 #include <unistd.h>
 #include <elf.h>
 
-void print_error(const char *msg) {
+void print_error(const char *msg)
+{
 	fprintf(stderr, "%s\n", msg);
 	exit(98);
 }
 
-void print_elf_header(const char *filename) {
+void print_elf_header(const char *filename)
+{
 	int fd = open(filename, O_RDONLY);
-	if (fd == -1) {
+
+	if (fd == -1)
+	{
 		perror("Error opening file");
 		exit(98);
 	}
 
 	Elf64_Ehdr header;
 
-	// Read ELF header
-	if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+	/* Read ELF header */
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
 		perror("Error reading ELF header");
 		close(fd);
 		exit(98);
 	}
 
-	// Check if it's an ELF file
+	/* Check if it's an ELF file */
 	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
 		header.e_ident[EI_MAG1] != ELFMAG1 ||
 		header.e_ident[EI_MAG2] != ELFMAG2 ||
-		header.e_ident[EI_MAG3] != ELFMAG3) {
+		header.e_ident[EI_MAG3] != ELFMAG3)
+	{
 		print_error("Not an ELF file");
 	}
 
-	// Print ELF header information
+	/* Print ELF header information */
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; ++i) {
+	for (int i = 0; i < EI_NIDENT; ++i)
+	{
 		printf("%02x ", header.e_ident[i]);
 	}
 	printf("\n");
@@ -48,14 +55,22 @@ void print_elf_header(const char *filename) {
 	printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
 
 	printf("  Type:                              ");
-	switch (header.e_type) {
-		case ET_EXEC: printf("EXEC (Executable file)\n"); break;
-		case ET_DYN:  printf("DYN (Shared object file)\n"); break;
-		default:      printf("<unknown: %x>\n", header.e_type); break;
+	switch (header.e_type)
+	{
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", header.e_type);
+		break;
 	}
 
-	// Use lseek to move to the entry point address
-	if (lseek(fd, (off_t)header.e_entry, SEEK_SET) == -1) {
+	/* Use lseek to move to the entry point address */
+	if (lseek(fd, (off_t)header.e_entry, SEEK_SET) == -1)
+	{
 		perror("Error using lseek");
 		close(fd);
 		exit(98);
@@ -64,12 +79,14 @@ void print_elf_header(const char *filename) {
 	close(fd);
 }
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
 		print_error("Usage: elf_header elf_filename");
 	}
 
 	print_elf_header(argv[1]);
 
-	return 0;
+	return (0);
 }
