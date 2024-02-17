8d4e636c96cf48123d34930b93b5ed1c377c613f
WARRIOR-MMA <warriorprishen@icloud.com>
2024-02-17 02:07:29 -0800

    Rectified 100-elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,6 +10,45 @@ void print_error(const char *msg)
 	exit(98);
 }
 
+void print_magic(const unsigned char *ident)
+{
+	printf("  Magic:   ");
+	for (int i = 0; i < EI_NIDENT; ++i)
+	{
+		printf("%02x ", ident[i]);
+	}
+	printf("\n");
+}
+
+void print_ident_info(const Elf64_Ehdr *header)
+{
+	printf("  Class:                             %s\n",
+			(header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n",
+			(header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n",
+			(header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" : "UNIX - NetBSD");
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+}
+
+void print_type_info(const Elf64_Ehdr *header)
+{
+	printf("  Type:                              ");
+	switch (header->e_type)
+	{
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_DYN:
+			printf("DYN (Shared object file)\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header->e_type);
+			break;
+	}
+}
+
 void print_elf_header(const char *filename)
 {
 	int fd = open(filename, O_RDONLY);
@@ -41,32 +80,9 @@ void print_elf_header(const char *filename)
 
 	/* Print ELF header information */
 	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; ++i)
-	{
-		printf("%02x ", header.e_ident[i]);
-	}
-	printf("\n");
-
-	printf("  Class:                             %s\n", (header.e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n", (header.e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n", (header.e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" : "UNIX - NetBSD");
-	printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
-
-	printf("  Type:                              ");
-	switch (header.e_type)
-	{
-	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
-		printf("DYN (Shared object file)\n");
-		break;
-	default:
-		printf("<unknown: %x>\n", header.e_type);
-		break;
-	}
+	print_magic(header.e_ident);
+	print_ident_info(&header);
+	print_type_info(&header);
 
 	/* Use lseek to move to the entry point address */
 	if (lseek(fd, (off_t)header.e_entry, SEEK_SET) == -1)
